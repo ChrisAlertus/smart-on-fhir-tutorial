11b10979fc3dcbe7154229b017f8455c60fdf7cb
Chris Alert <c.alert44@gmail.com>
2025-12-12 21:57:01 -0400

    Update example-smart-app.js
    fix name once and for all

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -33,12 +33,12 @@
 
           if (typeof patient.name[0] !== 'undefined') {
             console.log(patient.name[0])
-            const lname = Array.isArray(patient.name.family) 
-              ? patient.name.family.join(" ") 
-              : (patient.name.family || "");
-            const fname = Array.isArray(patient.name.given) 
-              ? patient.name.given.join(" ") 
-              : (patient.name.given || "");
+            const lname = Array.isArray(patient.name[0].family) 
+              ? patient.name[0].family.join(" ") 
+              : (patient.name[0].family || "");
+            const fname = Array.isArray(patient.name[0].given) 
+              ? patient.name[0].given.join(" ") 
+              : (patient.name[0].given || "");
           }
 
           var height = byCodes('8302-2');
